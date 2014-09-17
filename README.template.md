7f4698a61829b968ae34d744e4ff0515a5deb240
Richard Slater <richard.slater@amido.com>
2014-09-17 18:03:43 +0100

    Added a Metadata Check

---
+++ README.template.md
@@ -0,0 +1,9 @@
+Azure Vector Icons
+==================
+
+A set of vector (SVG) icons depicting Microsoft Azure Platform Services. Designed as a common icongraphic language for use by Architects, Developers and Operations to document and build Azure Platform Services.
+
+The Icons
+=========
+
+{{ICONS}}
--- default.ps1
+++ default.ps1
@@ -1,14 +1,42 @@
-Task default -Depends Render
+Task default -Depends "Inspect Metadata", Render
 
 Task CreateRendersFolder -PreCondition { -Not (Test-Path "$PSScriptRoot\renders") } {
   New-Item -ItemType Container "$PSScriptRoot\renders";
+}
+
+Task "Inspect Metadata" {
+  $icons = Get-ChildItem "$PSScriptRoot\icons";
+  $iconXml = $icons | Select-Object BaseName, Name, @{ Name="SVGDocument"; Expression={[xml](Get-Content $_.FullName);} };
+  [bool]$hasFailed = $false;
+
+  foreach ($icon in $iconXml) {
+    try {
+      $namespaceManager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $icon.SVGDocument.NameTable;
+      $icon.SVGDocument.SelectNodes('//namespace::*[not(. = ../../namespace::*)]') `
+        | Where-Object { $_.LocalName -ne 'xmlns' } `
+        | ForEach-Object { $namespaceManager.AddNamespace($_.LocalName, $_.Value) }
+      $metadata = $icon.SVGDocument.SelectSingleNode('//svg:svg/svg:metadata/rdf:RDF/cc:work', $namespaceManager);
+      $metadata;
+      Write-Host -NoNewLine -ForegroundColor Green "  [SUCCESS] "
+      Write-Host -ForegroundColor Yellow "$($icon.BaseName)";
+  } catch {
+      Write-Host -NoNewLine -ForegroundColor Red "  [FAILED ] "
+      Write-Host -ForegroundColor Yellow "$($icon.BaseName)";
+      Write-Host -ForegroundColor Red "    " + $_.ToString();
+      $hasFailed = $true;
+    }
+  }
+
+  if ($hasFailed) {
+    throw "One or more files faile metadata validation, please review the logs and correct validation as required.";
+  }
 }
 
 Task DeleteRenders -Depends CreateRendersFolder { 
   Get-ChildItem -Path "$PSScriptRoot\renders" -Filter "*.png" | Remove-Item;
 }
 
-Task Render -Depends DeleteRenders {
+Task Render -Depends "Inspect Metadata", DeleteRenders {
   if (Test-Path $env:ProgramFiles\Inkscape) {
     $inkscape = Join-Path $env:ProgramFiles "Inkscape\inkscape.com";
   }
@@ -31,7 +59,7 @@ Task Render -Depends DeleteRenders {
     $outputParameter = "--export-png=$outputDocument";
     $additionalParameters = @("--export-area-page", "--export-background-opacity=0.0");
   
-    Write-Host -ForegroundColor Magenta "Rendering $($svgDocument.BaseName)";
+    Write-Host -ForegroundColor Magenta "  Rendering $($svgDocument.BaseName)";
     & $inkscape $inputParameter $outputParameter $additionalParameters; 
   }
 }
